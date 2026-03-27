af5290570db5783047663eece215d7354406e390
vtu26046 <vtu26046@veltech.edu.in>
2026-03-27 09:27:57 +0530

    Time: 0 ms (100%), Space: 43.4 MB (79.37%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,28 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        // both null
+        if (t1 == null && t2 == null) return true;
+
+        // one null
+        if (t1 == null || t2 == null) return false;
+
+        // values must match + mirror structure
+        return (t1.val == t2.val)
+            && isMirror(t1.left, t2.right)
+            && isMirror(t1.right, t2.left);
+    }
+}
